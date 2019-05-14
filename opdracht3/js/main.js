ea1184000f4d30380107f8dce86cb88ba2248e85
Jason <kevinblank72@gmail.com>
2019-05-14 13:33:50 +0200

    javascript code bijgewerkt maar nog niet werkend. html update onderweg!!

---
+++ opdracht3/js/main.js
@@ -0,0 +1,33 @@
+var array = [];
+var array1 = [];
+
+var result = document.getElementById('field1');
+var result = document.getElementById('field2');
+
+function name() {
+  var name = document.querySelector("#name").value;
+  array.push(name);
+  result.innerHTML = "<h1>" + array[0] + "</h1><h1>" + array[1] + "</h1><h1>" + array[2] + "</h1><h1>" +
+    array[3] + "</h1><h1>" + array[4] + "</h1><h1>" + array[5] + "</h1><h1>" + array[6] + "</h1><h1>" + array[7] + "</h1>";
+}
+
+function switch1() {
+  array1.push(array[0]);
+  array.shift();
+
+  result.innerHTML = "<h1>" + array[0] + "</h1><h1>" + array[1] + "</h1><h1>" + array[2] + "</h1><h1>" +
+    array[3] + "</h1><h1>" + array[4] + "</h1><h1>" + array[5] + "</h1><h1>" + array[6] + "</h1><h1>" + array[7] + "</h1>";
+  result1.innerHTML = "<h1>" + array1[0] + "</h1><h1>" + array1[1] + "</h1><h1>" + array1[2] + "</h1><h1>" +
+    array1[3] + "</h1><h1>" + array1[4] + "</h1><h1>" + array1[5] + "</h1><h1>" + array1[6] + "</h1><h1>" + array1[7] + "</h1>";
+
+}
+
+function switch2() {
+  array1.push(array1[0]);
+  array.shift(0);
+
+  result.innerHTML = "<h1>" + array[0] + "</h1><h1>" + array[1] + "</h1><h1>" + array[2] + "</h1><h1>" +
+    array[3] + "</h1><h1>" + array[4] + "</h1><h1>" + array[5] + "</h1><h1>" + array[6] + "</h1><h1>" + array[7] + "</h1>";
+  result1.innerHTML = "<h1>" + array1[0] + "</h1><h1>" + array1[1] + "</h1><h1>" + array1[2] + "</h1><h1>" +
+    array1[3] + "</h1><h1>" + array1[4] + "</h1><h1>" + array1[5] + "</h1><h1>" + array1[6] + "</h1><h1>" + array1[7] + "</h1>";
+}
